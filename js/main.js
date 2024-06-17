4bc26d261f327d3ae5ad0cb096b1204860977196
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2024-06-17 09:49:59 +0200

    trying js fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var center = document.querySelector(".window-center");
-var mobileDevice = document.querySelector(".mobile-device-warning");
-if (window.outerWidth < 800)
-{          
-  center.style.display = "none";
-  mobileDevice.style.display = "block";
-}
-else
-{
-  mobileDevice.style.display = "none";
-}
+document.addEventListener("DOMContentLoaded", function() {
+  var center = document.querySelector(".window-center");
+  var mobileDevice = document.querySelector(".mobile-device-warning");
+
+  if (window.outerWidth < 800) {          
+      center.style.display = "none";
+      mobileDevice.style.display = "block";
+  } else {
+      mobileDevice.style.display = "none";
+  }
+});
